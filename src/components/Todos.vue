e6e1f170bf4222aa8b2610871fee8c407b18b414
quangminh <quangminh@gmail.com>
2023-05-11 01:00:18 +0700

    br02_props_directives

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,43 @@
 <template>
-  <p>Viec 1</p>
-  <p>Viec 2</p>
-  <p>Viec 3</p>
+  <TodoItem
+    v-for="todo in todos"
+    v-bind:key="todo.id"
+    v-bind:todoProps="todo"
+  />
 </template>
 
 <script>
+import { ref } from "vue";
+import TodoItem from "./TodoItem.vue";
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Todos",
+  components: {
+    TodoItem,
+  },
+  setup() {
+    const todos = ref([
+      {
+        id: 1,
+        title: "Viec 1",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Viec 2",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Viec 3",
+        completed: false,
+      },
+    ]);
+    return {
+      todos,
+    };
+  },
 };
 </script>
 
